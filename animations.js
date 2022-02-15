cdd9943c3ea048a5b803bfcdf96100996f4c1404
Tedd Chee <jchee86@gmail.com>
2022-02-15 12:56:30 -0600

    add images and update

---
+++ animations.js
@@ -0,0 +1,17 @@
+gsap.from(".sns-link", {
+  duration: 1,
+  scale: 0.5,
+  opacity: 0,
+  delay: 0.5,
+  stagger: 0.3,
+  ease: "elastic",
+})
+
+gsap.from(".button-link", {
+  duration: 1,
+  scale: 0.5,
+  opacity: 0,
+  delay: 0.25,
+  stagger: 0.2,
+  ease: "elastic", 
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
     fontSize: {
       xs: ['10px', '14px'],
       sm: ['12px', '16px'],
+      md: ['14px', '20px'],
+      lg: ['20px', '24px'],
+      xl: ['24px', '28px'],
+      xxl: ['28px', '32px'],
+      xxxl: ['32px', '36px'],
     }
   },
   plugins: [
